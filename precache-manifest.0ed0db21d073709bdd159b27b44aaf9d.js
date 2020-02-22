e1ca68dab51c379e8e8cbe84a5b8b1421a35186f
kate <klipsakate@gmail.com>
2020-02-22 16:52:41 -0500

    Updates

---
+++ precache-manifest.0ed0db21d073709bdd159b27b44aaf9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d2eb25928c2578267ed49040a598fc",
+    "url": "/React-counter-app/index.html"
+  },
+  {
+    "revision": "8b9ad75591b466b736cb",
+    "url": "/React-counter-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aca82bfa409cb19c1076",
+    "url": "/React-counter-app/static/js/2.6924ed70.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-counter-app/static/js/2.6924ed70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9ad75591b466b736cb",
+    "url": "/React-counter-app/static/js/main.9b67684b.chunk.js"
+  },
+  {
+    "revision": "870f423907c8c31cd769",
+    "url": "/React-counter-app/static/js/runtime-main.f0d882b0.js"
+  }
+]);
